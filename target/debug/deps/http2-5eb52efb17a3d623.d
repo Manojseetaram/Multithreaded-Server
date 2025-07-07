1992c9dd173e1fd3e9960381e2fb92dc0e1791ae
manojseetaram <manojseetaram.artytech@gmail.com>
2025-07-07 21:58:29 +0530

    Ignore the target folder

--- target/debug/deps/http2-5eb52efb17a3d623.d
+++
@@ -1,5 +0,0 @@
-/home/manojseetaram/Projects/Cargo/server/http2/target/debug/deps/libhttp2-5eb52efb17a3d623.rmeta: src/main.rs
-
-/home/manojseetaram/Projects/Cargo/server/http2/target/debug/deps/http2-5eb52efb17a3d623.d: src/main.rs
-
-src/main.rs:
